6079b035bf401f69c604e94a567c27fd1c85fed8
Divyank Bhalla <81313107+divyankbhalla@users.noreply.github.com>
2021-10-18 15:00:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,9 @@ let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
 }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
