ac28c0ec799ebfa29178419747eed55ea1a26782
Divyank Bhalla <81313107+divyankbhalla@users.noreply.github.com>
2021-10-18 13:51:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+}
