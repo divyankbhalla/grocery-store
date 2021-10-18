6a90ccc42b290f51118a6a58417b4b8582847963
Divyank Bhalla <81313107+divyankbhalla@users.noreply.github.com>
2021-10-18 15:05:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,41 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    cart.classList.remove('active');
 }
 
 let cart = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cart.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
 }
